a28a59d93edd618a28c5fad4ca61e15fd7ee9f6d
st3ph911 <st3ph911@gmail.com>
2020-08-20 22:01:00 +0000

    Upload New File

---
+++ menu_administrador.dart
@@ -0,0 +1,133 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:GFAS/constants.dart';
+import 'package:email_validator/email_validator.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
+
+class MenuAdministrador extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Colors.grey,
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              BotaoX(),
+              BotaoChamarBombeiro(),
+              BotaoSugestao(),
+              BotaoMeuPerfil(),
+              BotaoFAQ(),
+            ],
+        ),
+    );
+  }
+}
+
+class BotaoX extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+        onPressed: () {
+          Navigator.of(context).pushNamed('/menuAdministrador');
+        },
+        child: Align(
+          alignment: Alignment.topRight,
+          child: Container(
+            color: Colors.black26,
+            width: BOTAO_X_LARGURA,
+            height: BOTAO_X_ALTURA,
+            child: Center(
+              child: Text('X', style: TextStyle(fontSize: 28)),
+            ),
+          ),
+        )
+        );
+  }
+}
+
+class BotaoChamarBombeiro extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+        onPressed: () {
+          //Navigator.of(context).pushNamed('/menuAdministrador');
+        },
+        child: Container(
+          color: Colors.red,
+          margin: const EdgeInsets.all(10.0),
+          child: Center(
+            child: Text('Chamar Bombeiros', style: TextStyle(fontSize: 28, color: Colors.white)),
+          ),
+          width: MENU_BOTAO_LARGURA,
+          height: MENU_BOTAO_ALTURA,
+        ),
+    );
+  }
+}
+
+class BotaoSugestao extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      onPressed: () {
+        //Navigator.of(context).pushNamed('/menuAdministrador');
+      },
+      child: Container(
+        color: Colors.black26,
+        margin: const EdgeInsets.all(10.0),
+        child: Center(
+          child: Text('Sugestao', style: TextStyle(fontSize: 28, color: Colors.white)),
+        ),
+        width: MENU_BOTAO_LARGURA,
+        height: MENU_BOTAO_ALTURA,
+      ),
+    );
+  }
+}
+
+class BotaoMeuPerfil extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      onPressed: () {
+        //Navigator.of(context).pushNamed('/menuAdministrador');
+      },
+      child: Container(
+        color: Colors.black26,
+        margin: const EdgeInsets.all(10.0),
+        child: Center(
+          child: Text('Meu Perfil', style: TextStyle(fontSize: 28, color: Colors.white)),
+        ),
+        width: MENU_BOTAO_LARGURA,
+        height: MENU_BOTAO_ALTURA,
+      ),
+    );
+  }
+}
+
+class BotaoFAQ extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      onPressed: () {
+        //Navigator.of(context).pushNamed('/menuAdministrador');
+      },
+      child: Container(
+        color: Colors.black26,
+        margin: const EdgeInsets.all(10.0),
+        child: Center(
+          child: Text('FAQ', style: TextStyle(fontSize: 28, color: Colors.white)),
+        ),
+        width: MENU_BOTAO_LARGURA,
+        height: MENU_BOTAO_ALTURA,
+      ),
+    );
+  }
+}
+
+
+
