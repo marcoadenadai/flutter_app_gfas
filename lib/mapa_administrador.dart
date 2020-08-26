6cec976d21da941fa933014e5179db80837464f1
st3ph911 <st3ph911@gmail.com>
2020-08-26 14:34:45 +0000

    Upload New File

---
+++ lib/mapa_administrador.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:screenshot/screenshot.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
+
+class MapaAdministrador extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() => MapaAdministradorState();
+}
+
+class MapaAdministradorState extends State<MapaAdministrador> {
+
+}
+
+
