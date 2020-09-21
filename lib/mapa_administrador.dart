fccdf0acd44ef57284229a08091e39a14abe9409
st3ph911 <st3ph911@gmail.com>
2020-09-21 00:35:29 +0000

    Replace mapa_administrador.dart

--- lib/mapa_administrador.dart
+++ lib/mapa_administrador.dart
@@ -2,18 +2,61 @@ import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
-import 'package:screenshot/screenshot.dart';
-import 'package:image_gallery_saver/image_gallery_saver.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class MapaAdministrador extends StatefulWidget {
+
   @override
   State<StatefulWidget> createState() => MapaAdministradorState();
 }
 
 class MapaAdministradorState extends State<MapaAdministrador> {
-  @override
+@override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Container(
+      color: Colors.grey,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          RaisedButton(
+            onPressed: _launchURL,
+            child: Text('Ver Mapa'),
+          ),
+          RaisedButton(
+            onPressed: () {
+              Navigator.of(context).pushNamed('/menuAdministrador');
+            },
+            child: Text('Menu Administrador'),
+          ),
+        ],
+      ),
+    );
+
+  /*Scaffold(
+      body: Center(
+        children: [
+          RaisedButton(
+            onPressed: _launchURL,
+            child: Text('Ver Mapa'),
+          ),
+          RaisedButton(
+            onPressed: _launchURL,
+            child: Text('Menu Administrador'),
+          ),
+        ]
+      ),
+    );*/
+  }
+_launchURL() async {
+  const url = 'https://www.google.com.br/maps';
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
   }
 }
+}
+
+
+
+
